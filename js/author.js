380def8df833c06c907c3a30c24da3d070e02d56
Swe Zin Thant <EvyT@MizzyyEvy-4.local>
2016-05-06 00:57:36 -0700

    Week04 Homework.

---
+++ js/author.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	var answer = prompt ("Hello");
+
+switch (answer) {
+  case "Hello":
+    alert("Hello there!");
+    break;
+  case "How are you?":
+    alert("I am good.");
+    break;
+  case "How is you day?":
+    alert("so far, so good.");
+    break;
+}
+
+function bb(bee,honey) {
+  if (bee === honey) {
+    return true;
+  } else {
+    return false;
+}
+  
+
+ var logodesign = {
+ 	"logo": "swezinery",
+ 	"colors": 2,
+ 	"size": "square",
+ 	"style" : ["typography", "geometric", "qrcode", "asian"]
+};
+	logo.name = "Swe Zin Thant"
+	logodesign["edge"] = "round";
+
+	delete logodesign.size;
+
+	things.hasownProperty("logo");
+	things.hasownProperty("typeface");
+}};
